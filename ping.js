dace2a59bbbaab770e4c8d4340d6c85ae63b8a46
clement chan <clement.chan@bluescape.com>
2018-12-19 21:46:20 -0800

    add ping to api posts

--- ping.js
+++ ping.js
@@ -1,24 +1,25 @@
 var http = require('http'); //importing http
+var log4js = require('log4js');
+log4js.configure({
+    appenders: { ubuntu: { type: 'file', filename: 'ping.log' } },
+    categories: { default: { appenders: ['ubuntu'], level: 'info' } }
+  });
+var logger = log4js.getLogger('ubuntu');
+var axios = require('axios');
 
 function startKeepAlive() {
-    setInterval(function() {
-        var options = {
-            host: 'https://warm-garden-52058.herokuapp.com',
-            port: 80,
-            path: '/'
-        };
-        http.get(options, function(res) {
-            res.on('data', function(chunk) {
-                try {
-                    // optional logging... disable after it's working
-                    console.log("HEROKU RESPONSE: " + chunk);
-                } catch (err) {
-                    console.log(err.message);
-                }
-            });
-        }).on('error', function(err) {
-            console.log("Error: " + err.message);
-        });
+    setInterval(async () => {
+        let res;
+        try {
+            res = await axios.get('https://warm-garden-52058.herokuapp.com/api/posts');
+            if (res.status === 200) {
+                logger.info(`/api/posts has ${res.data.length} entries`);
+            } else {
+                logger.error('/api/posts failure')
+            }
+        } catch(err) {
+            logger.error(err);
+        }
     }, 20 * 60 * 1000); // load every 20 minutes
 }
 
